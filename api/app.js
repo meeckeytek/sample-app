4028017d4eab5a32590dd1239c5a325637359970
Ubuntu <meeckey@meeckey.00vtpwadunnu3ktg35e4scmtsf.gx.internal.cloudapp.net>
2023-08-07 19:20:23 +0000

    kubernetes implementation

--- api/app.js
+++ api/app.js
@@ -3,22 +3,13 @@ const mongoose = require("mongoose");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const path = require("path");
 const postRouter = require("./routes/postRoute");
-// const cors = require("cors");
+const cors = require("cors");
 
 require("dotenv").config();
 
 const app = express();
 
-app.use(express.static(path.join("public")));
-
-app.use(express.json({ limit: "50mb" }));
-// app.use(express.json());
-
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
-
-// app.use(cors());
-
-app.use("/", express.static(path.join("uploads")));
+app.use(cors())
 
 // //Making sure all requested are accessed and header are set
 app.use((req, res, next) => {
@@ -30,6 +21,17 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
   next();
 });
+
+app.use(express.static(path.join("public")));
+
+app.use(express.json({ limit: "50mb" }));
+// app.use(express.json());
+
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+// app.use(cors());
+
+app.use("/", express.static(path.join("uploads")));
 
 // //Using user route
 app.use("/api/v1/post", postRouter);
--- ui/src/axios.js
+++ ui/src/axios.js
@@ -1,21 +1,7 @@
 import axios from "axios";
 
-const AUTH_TOKEN = localStorage.getItem("userToken");
-
 const api = axios.create({
-  // baseURL: "http://localhost:5011/api/v1",
-  baseURL: "http://localhost:8000/api/v1/post",
+  baseURL: `http://${api-service.default}:8000/api/v1/post`,
 });
 
 export default api;
-
-// //All Posts
-// export const allPosts = () => ;
-
-// //New Post
-// export const newUser = (values) =>
-//  
-
-// //Post details
-// export const deleteUser = (postId) =>
-//   api.delete(`/${postId}`).then((res) => res.data);
